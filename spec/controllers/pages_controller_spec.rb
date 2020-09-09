66a488f0a461388f78972175d8b6e6c2a42b11b6
Konstantin <konstantin.faleev@gmail.com>
2020-09-09 21:10:56 +0300

    rename home page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PagesController do
-  describe 'GET /home' do
+  describe 'GET /index' do
     it '200 status code' do
-      get :home
+      get :index
       expect(response).to have_http_status(:success)
     end
   end
